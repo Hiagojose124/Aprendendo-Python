8f2af3c577c91f3ca71dae4cf327031806fcb8b7
Hiago José <hiago.araujo.dev@gmail.com>
2026-08-27 15:55:49 -0300

    Add user greeting and birth date prompts

--- aula-04-desafios.py
+++ aula-04-desafios.py
@@ -1,1 +1,9 @@
+#Desafio 01: Boas-vindas
+nome=input('Qual é o seu nome?')
+print('Olá', nome,'!Prazer em conhecer!')
 
+#Desafio 02: Data de nascimento
+dia=input('Dia:')
+mes=input('Mês:')
+ano=input('Ano:')
+print('Você nasceu no dia', dia,'de', mes,'de',ano,'.Correto?')
