dcc2dc071c1dd74b794ed584777165a58b352ade
Hiago José <hiago.araujo.dev@gmail.com>
2026-08-31 17:09:56 -0300

    Add program to sum two integers
    Implement a program that reads two integers and displays their sum.

---
+++ aula06-desefio03-soma.py
@@ -0,0 +1,8 @@
+#Desafio 003-Somando dois números 
+#Curso em Vídeo-Python
+#Resolução: lê dois números inteiros e mostra a soma entre eles
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print('A soma vale', s)
